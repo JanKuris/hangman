8639e73c007d39e24d8fa698b204e7a01e93ece4
Jan Kuris <jan.kuris@gmail.com>
2024-01-30 19:06:24 +0100

    inicial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import random
+
+word_list = ["dog", "pencil"]
+chosen_word = random.choice(word_list)
+# chosen_word = list(random_word)
+word_lenght = len(chosen_word)
+
+display = []
+for _ in range(word_lenght):
+    display += "_"
+print(display)
+
+
+user_input = input(f"Guess a letter:\n").lower()
+    
+for position in range(word_lenght) :
+    letter = chosen_word[position]
+    if letter == user_input:
+        display[position] = letter
+    
+print(display)
