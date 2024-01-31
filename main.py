9cc8e3cd1f5f486414e5bfbbf4c4dad98cb947da
Jan Kuris <jan.kuris@gmail.com>
2024-01-31 23:52:18 +0100

    while loop is ready and win function too

--- main.py
+++ main.py
@@ -1,24 +1,31 @@
 import random
 
 
-word_list = ["dog", "pencil"]
+word_list = ["dog", "pencil", "baboon"]
 chosen_word = random.choice(word_list)
-# chosen_word = list(random_word)
 word_lenght = len(chosen_word)
+letter_list = list(chosen_word)
+print(chosen_word)
 
 display = []
 for _ in range(word_lenght):
     display += "_"
 print(display)
 
-while chosen_word != display:
+
+while display != letter_list:
     user_input = input(f"Guess a letter:\n").lower()
         
-    for position in range(word_lenght) :
+    for position in range(word_lenght):
         letter = chosen_word[position]
         if letter == user_input:
             display[position] = letter
-    
-        
-
+ 
+    if not display == display:
+        break
     print(display)
+    print("win")
+
+  
+
+       
